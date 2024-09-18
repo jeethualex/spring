1378ed96984e54656b81d9a90135b0661a00e1a5
G 2 <jeethualex@gmail.com>
2024-09-18 17:01:02 +0100

    Spring security added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,4 +16,9 @@ public class DemoApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	@GetMapping("/admin")
+	public String admin(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
