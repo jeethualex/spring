27740456861091128d3f77edf0210d1c153e53c8
G 2 <jeethualex@gmail.com>
2024-09-20 21:51:26 +0100

    jdbc template added

--- src/main/java/com/example/demo/jdbc/CustomerController.java
+++ src/main/java/com/example/demo/jdbc/CustomerController.java
@@ -1,7 +1,5 @@
 package com.example.demo.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
-
-	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 
 	@Autowired
 	private CustomerUtils customerUtils;
