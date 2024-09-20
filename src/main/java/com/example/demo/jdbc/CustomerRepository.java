0a2ec2f869daf739dcb81ce7cd6594b85fb32789
G 2 <jeethualex@gmail.com>
2024-09-20 20:53:36 +0100

    jdbc template added

--- src/main/java/com/example/demo/jdbc/CustomerRepository.java
+++ src/main/java/com/example/demo/jdbc/CustomerRepository.java
@@ -2,9 +2,13 @@ package com.example.demo.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,6 +18,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class CustomerRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomerRepository.class);
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
@@ -57,5 +64,23 @@ public class CustomerRepository {
                 new Object[] {
                         employee.getFirstName(), employee.getLastName(), employee.getId()
                 });
+    }
+
+    public void createData() {
+        log.info("Creating tables");
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS customers(" +
+                "id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255), last_name VARCHAR(255), PRIMARY KEY (id))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
     }
 }
--- src/main/java/com/example/demo/jdbc/CustomerUtils.java
+++ src/main/java/com/example/demo/jdbc/CustomerUtils.java
@@ -3,12 +3,7 @@ package com.example.demo.jdbc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class CustomerUtils {
@@ -16,27 +11,10 @@ public class CustomerUtils {
 	private static final Logger log = LoggerFactory.getLogger(CustomerUtils.class);
 
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-
-	@Autowired
 	private CustomerRepository customerRepository;
 
 	public void dbCreateOperations() {
-		log.info("Creating tables");
-		jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
-		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS customers(" +
-				"id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255), last_name VARCHAR(255), PRIMARY KEY (id))");
-
-		// Split up the array of whole names into an array of first/last names
-		List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-				.map(name -> name.split(" "))
-				.collect(Collectors.toList());
-
-		// Use a Java 8 stream to print out each tuple of the list
-		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
-		// Uses JdbcTemplate's batchUpdate operation to bulk load data
-		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+		customerRepository.createData();
 	}
 
 	public void dbCheckOperations() {
