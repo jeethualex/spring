2fab97792d111812e9c164f6ff1c2822c64c2887
G 2 <jeethualex@gmail.com>
2024-09-22 19:05:23 +0100

    jdbc template added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.jdbc.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private CustomerService customerService;
+
 	@Test
 	void contextLoads() {
+
+		customerService.dbCreateOperations();
+		customerService.dbCheckOperations();
+		customerService.dbCheckRestOperations();
 	}
 
 }
