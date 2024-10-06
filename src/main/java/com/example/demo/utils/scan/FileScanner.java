26112e60f30d72640883c51a0d29b925ff70eb26
G 2 <jeethualex@gmail.com>
2024-10-06 20:00:22 +0100

    File scanner added

--- src/main/java/com/example/demo/utils/scan/FileScanner.java
+++ src/main/java/com/example/demo/utils/scan/FileScanner.java
@@ -11,54 +11,13 @@ public class FileScanner {
     public static final String out = "C:\\Users\\j\\Documents\\out.csv";
     public static final String scanPath = "C:\\";
 
-    public static StringBuffer sb;
-
     public static void main(String[] args) throws IOException {
 
         clearfile("", out);
-        sb = getListRet(scanPath, new StringBuffer());
-        //getList(scanPath);
-        writefile(sb.toString(), out);
-        //writefile("in", "C:\\Users\\j\\Documents\\out.txt");
+        writefile(getListRet(scanPath, new StringBuffer()).toString(), out);
     }
 
-    public static void getList(String path) throws IOException {
-        //  System.out.println("Hello jeetu!");
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
-        if (listOfFiles != null) {
-            for (int i = 0; i < listOfFiles.length; i++) {
-                if (listOfFiles[i].isFile()) {
-
-                    //Date date = new Date(listOfFiles[i].lastModified());
-
-                    //long fileSizeInBytes = listOfFiles[i].length();
-                    // Convert the bytes to Kilobytes (1 KB = 1024 Bytes)
-                    //long fileSizeInKB = fileSizeInBytes / 1024;
-                    // Convert the KB to MegaBytes (1 MB = 1024 KBytes)
-                    //long fileSizeInMB = fileSizeInKB / 1024;
-
-                    //String outString = "\""+listOfFiles[i].getPath()+"\"" + "," + "\""+listOfFiles[i].length()+"\"" + "," + "\""+listOfFiles[i].lastModified()+"\"" + "\n";
-
-                    // System.out.println(listOfFiles[i].getName());
-                    //writefile("\""+listOfFiles[i].getPath()+"\"" + "," + "\""+listOfFiles[i].length()+"\"" + "," + "\""+listOfFiles[i].lastModified()+"\"" + "\n", out);
-                    sb.append("\""+listOfFiles[i].getPath()+"\"" + "," + "\""+listOfFiles[i].length()+"\"" + "," + "\""+listOfFiles[i].lastModified()+"\"" + "\n");
-
-                    System.out.println(listOfFiles[i].getName());
-
-                    //System.out.println(writefileret("\""+listOfFiles[i].getPath()+"\"" + "," + "\""+listOfFiles[i].length()+"\"" + "," + "\""+listOfFiles[i].lastModified()+"\"" + "\n", out));
-
-                } else if (listOfFiles[i].isDirectory()) {
-                    //System.out.println("Directory " + listOfFiles[i].getName());
-                    //System.out.println("Directory " + listOfFiles[i].getPath());
-                    getList(listOfFiles[i].getPath());
-                }
-            }
-        }
-    }
-
-    public static StringBuffer getListRet(String path, StringBuffer sb) throws IOException {
-        //  System.out.println("Hello jeetu!");
+    public static StringBuffer getListRet(String path, StringBuffer sb) {
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
         if (listOfFiles != null) {
@@ -77,7 +36,6 @@ public class FileScanner {
         }
         return sb;
     }
-
 
     public static void writefile(String content, String path) throws IOException {
 
