3369335aee7b56d280548664a06497e4403b03b3
G 2 <jeethualex@gmail.com>
2024-10-07 09:35:17 +0100

    File scanner added

--- src/main/java/com/example/demo/utils/scan/FileScanner.java
+++ src/main/java/com/example/demo/utils/scan/FileScanner.java
@@ -7,50 +7,26 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class FileScanner {
-
-    public static final String out = "C:\\Users\\j\\Documents\\out.csv";
-    public static final String scanPath = "C:\\";
-
     public static void main(String[] args) throws IOException {
+        Files.write(
+                Paths.get("C:\\Users\\j\\Documents\\out.csv"),
+                getListRet("C:\\", new StringBuffer()).toString().getBytes(),
+                StandardOpenOption.TRUNCATE_EXISTING);
 
-        clearfile();
-        writefile(getListRet(scanPath, new StringBuffer()).toString());
     }
 
     public static StringBuffer getListRet(String path, StringBuffer sb) {
-
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
+        File[] listOfFiles = new File(path).listFiles();
         if (listOfFiles != null) {
             for (int i = 0; i < listOfFiles.length; i++) {
                 if (listOfFiles[i].isFile()) {
-
-                    sb.append("\""+listOfFiles[i].getPath()+"\"" + "," + "\""+listOfFiles[i].length()+"\"" + "," + "\""+listOfFiles[i].lastModified()+"\"" + "\n");
-
+                    sb.append("\"" + listOfFiles[i].getPath() + "\"" + "," + "\"" + listOfFiles[i].length() + "\"" + "," + "\"" + listOfFiles[i].lastModified() + "\"" + "\n");
                     System.out.println(listOfFiles[i].getName());
-
                 } else if (listOfFiles[i].isDirectory()) {
-
-                    getListRet(listOfFiles[i].getPath(),sb);
+                    getListRet(listOfFiles[i].getPath(), sb);
                 }
             }
         }
         return sb;
-    }
-
-    public static void writefile(String content) throws IOException {
-
-        Files.write(
-                Paths.get(out),
-                content.getBytes(),
-                StandardOpenOption.APPEND);
-    }
-
-    public static void clearfile() throws IOException {
-
-        Files.write(
-                Paths.get(out),
-                "".getBytes(),
-                StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
