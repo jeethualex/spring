ea2ab95a0120f2ab9414f95af76a7884dea84c77
G 2 <jeethualex@gmail.com>
2024-10-09 17:13:00 +0100

    Selenium test added

--- src/test/java/com/example/demo/SeleniumTests.java
+++ src/test/java/com/example/demo/SeleniumTests.java
@@ -4,17 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 public class SeleniumTests {
 
     private WebDriver driver;
 
     @Test
-    public void test1() throws InterruptedException {
-
+    public void test1() {
         WebDriver driver = new EdgeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://localhost:8080/swagger-ui/index.html");
-        Thread.sleep(10000);
-        driver.quit();
+
+        try {
+            driver.manage().window().maximize();
+            driver.get("http://localhost:8080/swagger-ui/index.html");
+            Thread.sleep(10000);
+            driver.quit();
+        } catch(Exception e) {
+            driver.quit();
+            assertTrue(e.getMessage(), false);
+        }
     }
 }
